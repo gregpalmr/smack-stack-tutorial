16498a901464854db181e2b87a269fab323bfc85
Greg Palmer <greg.palmer@mesosphere.com>
2017-12-05 13:24:08 -0500

    changed jar file path

--- scripts/run-sample-spark-kafka-job.sh
+++ scripts/run-sample-spark-kafka-job.sh
@@ -2,7 +2,8 @@
 #
 # SCRIPT: run-sample-spark-kafka-job.sh
 #
+# http://downloads.mesosphere.com/spark/assets/spark-examples_2.11-2.0.1.jar
 
-dcos spark run --submit-args='--conf spark.eventLog.enabled=true --conf spark.eventLog.dir=hdfs://hdfs/history --conf Dspark.mesos.coarse=true --conf spark.cores.max=4 --conf spark.executor.memory=1g --driver-cores 1 --driver-memory 1g --class org.apache.spark.examples.streaming.KafkaWordCount  https://downloads.mesosphere.com/spark/assets/spark-examples_2.10-1.4.0-SNAPSHOT.jar mesos://leader.mesos:5050 zk-1.zk,zk-2.zk,zk-3.zk my-consumer-group my-topic 1'
+dcos spark run --name="spark" --submit-args='--conf spark.eventLog.enabled=true --conf spark.eventLog.dir=hdfs://hdfs/history --conf Dspark.mesos.coarse=true --conf spark.cores.max=4 --conf spark.executor.memory=1g --driver-cores 1 --driver-memory 1g --class org.apache.spark.examples.streaming.KafkaWordCount  http://downloads.mesosphere.com/spark/assets/spark-examples_2.11-2.0.1.jar mesos://leader.mesos:5050 zk-1.zk,zk-2.zk,zk-3.zk my-consumer-group my-topic 1'
 
 # End of script

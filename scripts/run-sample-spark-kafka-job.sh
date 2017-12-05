6abe104a6ba6d662a31fdb1e4ca16a05200f4a67
Greg Palmer <greg.palmer@mesosphere.com>
2017-12-05 15:59:16 -0500

    Changed Spark Kafa Consumer example

--- scripts/run-sample-spark-kafka-job.sh
+++ scripts/run-sample-spark-kafka-job.sh
@@ -2,8 +2,23 @@
 #
 # SCRIPT: run-sample-spark-kafka-job.sh
 #
-# http://downloads.mesosphere.com/spark/assets/spark-examples_2.11-2.0.1.jar
 
-dcos spark run --name="spark" --submit-args='--conf spark.eventLog.enabled=true --conf spark.eventLog.dir=hdfs://hdfs/history --conf Dspark.mesos.coarse=true --conf spark.cores.max=4 --conf spark.executor.memory=1g --driver-cores 1 --driver-memory 1g --class org.apache.spark.examples.streaming.KafkaWordCount  http://downloads.mesosphere.com/spark/assets/spark-examples_2.11-2.0.1.jar mesos://leader.mesos:5050 zk-1.zk,zk-2.zk,zk-3.zk my-consumer-group my-topic 1'
+echo
+echo "#############################################"
+echo "### Running Spark Kafka Consumer example. ###"
+echo "#############################################"
+echo
+
+dcos spark run --name="spark" --submit-args='--conf spark.eventLog.enabled=true --conf spark.eventLog.dir=hdfs://hdfs/history --conf Dspark.mesos.coarse=true --conf spark.cores.max=4 --conf spark.executor.memory=1g --driver-cores 1 --driver-memory 1g --class KafkaConsumer  https://s3-us-west-2.amazonaws.com/arand-sandbox-mesosphere/beta-spark/dcos-spark-scala-tests-assembly-0.1-SNAPSHOT.jar broker.kafka.l4lb.thisdcos.directory:9092 my-topic 40 false'
+
+echo
+echo " Also running the command to genreate Kafka producer test data in topic my-topic. "
+echo " Watch the STDOUT for the Spark Driver task in the DC/OS Service->Spark Web page:"
+echo
+sleep 20
+dcos kafka topic producer_test my-topic 100
+dcos kafka topic producer_test my-topic 100
+dcos kafka topic producer_test my-topic 100
+echo
 
 # End of script

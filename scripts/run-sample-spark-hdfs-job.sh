34f3c77caa505502a407c91d6d3920be23052845
Greg Palmer <greg.palmer@mesosphere.com>
2017-12-13 08:38:12 -0500

    Fixed --conf typo

--- scripts/run-sample-spark-hdfs-job.sh
+++ scripts/run-sample-spark-hdfs-job.sh
@@ -9,6 +9,6 @@ echo "### Running Spark HDFSWordCount example. ###"
 echo "#############################################"
 echo
 
-dcos spark run --name 'spark' --submit-args='--conf spark.eventLog.enabled=true --conf spark.eventLog.dir=hdfs://hdfs/history --conf Dspark.mesos.coarse=true --conf spark.cores.max=4 --conf spark.executor.memory=2g --driver-cores 1 --driver-memory 1g --class HDFSWordCount https://s3-us-west-2.amazonaws.com/greg-palmer/smack-stack-tutorial/sparkjob-assembly-1.0.jar hdfs:///test-data/test-data.txt'
+dcos spark run --name 'spark' --submit-args='--conf spark.eventLog.enabled=true --conf spark.eventLog.dir=hdfs://hdfs/history --conf spark.mesos.coarse=true --conf spark.cores.max=4 --conf spark.executor.memory=2g --driver-cores 1 --driver-memory 1g --class HDFSWordCount https://s3-us-west-2.amazonaws.com/greg-palmer/smack-stack-tutorial/sparkjob-assembly-1.0.jar hdfs:///test-data/test-data.txt'
 
 # End of script
